5c6baad417d42083d833bfbf312177c0aff6858d
Maverick Tesini <mavericktesini@gmail.com>
2018-07-30 22:17:37 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: {in: [true, false]}
   # release_year must be set, if released == true
   #EXAMPLE:  validates :card_number, presence: true, if: :paid_with_card?
-  validates :release_year, presence: true, if: :released == true 
+  validates :release_year, presence: true, if: :released == true
   validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
   #must be this year or before.
 
